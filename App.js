639db57046b7c5a8673ef77c00ee96745263bab9
Mateus <maateusmania@gmail.com>
2023-03-03 09:46:12 -0300

    products initialized

--- App.js
+++ App.js
@@ -1,17 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Button } from '@rneui/themed';
 
 export default function App() {
+
+  const listProducts = ["Americano", "Café com leite"]
+
+
+  const Products = () => {
+    listProducts.map((i) => {
+      return(
+        <Product nome="Oi"/>
+      )
+    })
+  }
+
+
+
+  const Product = (props) => {
+        <View>
+          <Text>{props.nome}</Text>
+          <Text>Estado</Text>
+          <View style={{flex: 1, flexDirection: "row",  justifyContent: "space-between"}}>
+            <Button size="sm">P</Button>
+            <Button size="sm">M</Button>
+            <Button size="sm">G</Button>
+          </View>
+        </View>
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Coffee</Text>
+      <Products/>
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop:50,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
