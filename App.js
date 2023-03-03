8a570a00c397ec324962d27efb7704c434c4ac5f
Mateus <maatmds04@gmail.com>
2023-03-03 16:57:11 -0300

    hotfixed map to render products

--- App.js
+++ App.js
@@ -1,49 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { Button } from '@rneui/themed';
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default function App() {
 
-  const listProducts = ["Americano", "Café com leite"]
+  const Produto = (props) => {
+  
+    const handleAction = () => {
+      null
+    };
 
 
-  const Products = () => {
-    listProducts.map((i) => {
-      return(
-        <Product nome="Oi"/>
-      )
-    })
-  }
-
-
-
-  const Product = (props) => {
-        <View>
-          <Text>{props.nome}</Text>
-          <Text>Estado</Text>
-          <View style={{flex: 1, flexDirection: "row",  justifyContent: "space-between"}}>
-            <Button size="sm">P</Button>
-            <Button size="sm">M</Button>
-            <Button size="sm">G</Button>
-          </View>
+    return (
+      <View style={styles.produto}>
+        <Text>{props.nome}</Text>
+        <View style={styles.botoes}>
+          <TouchableOpacity style={styles.botaoSim} onPress={handleAction}><Text>Sim</Text></TouchableOpacity>
         </View>
-  }
+      </View>
+    );
+  };
 
-  return (
-    <View style={styles.container}>
-      <Text>Coffee</Text>
-      <Products/>
-    </View>
-  );
-
+  const Produtos = [
+    { id: 1, nome: 'Café com leite' },
+    { id: 2, nome: 'Cafe moccachino' },
+    { id: 3, nome: 'Café amarelo' },
+  ];
+  
+    return (
+      <View>
+        {Produtos.map((produto) => (
+          <Produto nome ={produto.nome} />
+        ))}
+      </View>
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop:50,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  produto: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  botoes: {
+    flexDirection: 'row',
+    marginTop: 10,
+  },
+  botaoSim: {
+    backgroundColor: 'green',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    marginRight: 10,
+    borderRadius: 5,
+  },
+  botaoNao: {
+    backgroundColor: 'red',
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+  textoBotao: {
+    color: '#fff',
   },
 });
